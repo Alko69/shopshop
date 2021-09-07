d54e1e5a536b9d1df7e89d5a4c435a8095551565
admin <admin@iMac-ITK-04.local>
2021-09-07 16:03:57 +0200

    range et toggle jordan

--- index.js
+++ index.js
@@ -1,4 +1,44 @@
-var imgs = document.getElementById('img');
-var test = imgs.getAttribute('src');
-console.log(test);
+// var imgs = document.getElementById('img');
+// var test = imgs.getAttribute('src');
+// console.log(test);
     
+var button = document.querySelector('#yes');
+
+button.addEventListener('click', el => {
+    el = el.target;
+    var test = document.querySelectorAll(".card");
+    // if( test.classList.contains('jordan')){
+    //     test.classList.toggle('none');
+    // }
+    test.forEach(element => {
+        if(element.classList.contains('jordan')){
+            element.classList.toggle('none');
+        }
+    });
+    
+})
+
+var essai = document.querySelector('#range');
+var button2 = document.querySelector('#no');
+var value = document.querySelector('#value');
+var price = document.querySelectorAll('.price');
+
+essai.addEventListener('input', (el) =>{
+    el = el.target;
+    var value = document.querySelector('#value');
+    value.innerHTML = el.value;
+})
+
+button2.addEventListener('click', () =>{
+    var increment = -1;
+    price.forEach(element => {
+        increment ++;
+        if(element.innerHTML > value.innerHTML){
+            document.querySelectorAll('.card')[increment].classList.add('none');
+        }
+        if(element.innerHTML < value.innerHTML){
+            document.querySelectorAll('.card')[increment].classList.remove('none');
+        }
+    });
+
+})
--- test.php
+++ test.php
@@ -8,20 +8,46 @@
 </head>
 <body>
     <header>
+        <button id="yes">yes</button>
+        <button id="no">no</button>
+        <p id="value">50</p>
+        <div id="button">
+            <p>0</p>
+            <input  id="range" type="range" min=0 max=100>
+            <p>100</p>
+        </div>
     </header>
     <div id="main">
-        <div class="card">
+        <div class="card jordan">
             <img id="img" src="./imges/img1.jpg" alt="">
             <div class="subtext">
                 <p>Marque</p>
                 <p>Prix</p>
+                <p class="price">50</p>
             </div>
         </div>
-        <div class="card">
+        <div class="card nike jordan">
             <img src="./imges/img2.jpeg" alt="">
             <div class="subtext">
                 <p>Marque</p>
                 <p>Prix</p>
+                <p class="price">72</p>
+            </div>
+        </div>
+        <div class="card">
+            <img src="./imges/img3.jpeg" alt="">
+            <div class="subtext">
+                <p>Marque</p>
+                <p>Prix</p>
+                <p class="price">27</p>
+            </div>
+        </div>
+        <div class="card">
+            <img src="./imges/img4.jpeg" alt="">
+            <div class="subtext">
+                <p>Marque</p>
+                <p>Prix</p>
+                <p class="price">40</p>
             </div>
         </div>
     </div>
